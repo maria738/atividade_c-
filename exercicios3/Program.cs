cdb17b717eae6e15fbcaf8a7708772cd6007e8e8
MARIA VITORIA MACHADO DE LIMA <maria.lima05@cs.up.edu.br>
2025-04-07 21:22:05 -0300

    exercicios3

---
+++ exercicios3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Digite a sua nota 1 : "); 
+float num1 = float.Parse(Console.ReadLine()); 
+
+Console.WriteLine("Digite a sua nota 2 : "); 
+float num2 = float.Parse(Console.ReadLine()); 
+
+Console.WriteLine("Digite a sua nota 3 : "); 
+float num3 = float.Parse(Console.ReadLine()); 
+
+float media = (num1 + num2 + num3) / 3;
+
+Console.WriteLine("A sua media de notas é: " + media);
